4d1de24961a5d28427c5f750c963ff69daf4920b
fabianperdomolaguna <faperdomola@gmail.com>
2022-06-11 22:32:18 -0500

    Add a ConfigFile.json and Color Theme selection in Main Menu

--- Example_App/UI_Layers/main_menu_bar.cpp
+++ Example_App/UI_Layers/main_menu_bar.cpp
@@ -1,6 +1,10 @@
 module;
 
+#include <fstream>
+#include <string>
+
 #include "imgui.h"
+#include "nlohmann/json.hpp"
 
 export module MainMenu;
 
@@ -9,9 +13,10 @@ import Application;
 struct menu_variables
 {
 	bool exit_item = false;
+	bool theme_item = false;
 };
 
-export void exit_popup(bool& exit_item, application *app)
+void exit_popup(bool &exit_item, application *app)
 {
 	if (exit_item)
 		ImGui::OpenPopup("Close the application?");
@@ -35,6 +40,42 @@ export void exit_popup(bool& exit_item, application *app)
 	}
 }
 
+void change_style_popup(bool &theme_item, application* app)
+{
+	if (theme_item)
+		ImGui::OpenPopup("App color theme");
+
+	// Always center this window when appearing
+	ImVec2 center = ImGui::GetMainViewport()->GetCenter();
+	ImGui::SetNextWindowPos(center, ImGuiCond_Appearing, ImVec2(0.5f, 0.5f));
+
+	if (ImGui::BeginPopupModal("App color theme", NULL, ImGuiWindowFlags_NoResize))
+	{
+		ImGui::Text("Close the app to aply theme changes.\n");
+		ImGui::Separator();
+
+		if (ImGui::Button("Ok", ImVec2(120, 0)))
+			ImGui::CloseCurrentPopup();
+
+		ImGui::EndPopup();
+	}
+
+}
+
+void change_app_style(std::string app_style)
+{
+	std::fstream json_config_file;
+	json_config_file.open("ConfigFile.json", std::ios::in);
+	nlohmann::json json_data = nlohmann::json::parse(json_config_file);
+	json_config_file.close();
+
+	json_data["GuiStyle"] = app_style;
+
+	json_config_file.open("ConfigFile.json", std::ios::out);
+	json_config_file << json_data;
+	json_config_file.close();
+}
+
 export void menu_example(application* app)
 {
 	menu_variables m_variables;
@@ -44,9 +85,26 @@ export void menu_example(application* app)
 		if (ImGui::MenuItem("Exit"))
 			m_variables.exit_item = true;
 
-		if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+		if (ImGui::BeginMenu("Color Theme"))
+		{
+			if (ImGui::MenuItem("Dark", ""))
+			{
+				change_app_style("dark");
+				m_variables.theme_item = true;
+			}
+
+			if (ImGui::MenuItem("Light", ""))
+			{
+				change_app_style("light");
+				m_variables.theme_item = true;
+			}
+
+			ImGui::EndMenu();
+		}
+
 		ImGui::EndMenu();
 	}
 
 	exit_popup(m_variables.exit_item, app);
+	change_style_popup(m_variables.theme_item, app);
 };
--- GUI_Core/Core/imgui_context.cpp
+++ GUI_Core/Core/imgui_context.cpp
@@ -1,14 +1,202 @@
 module;
 
+#include <string>
+#include <fstream>
+#include <unordered_map>
+#include <any>
+
 #include "GLFW/glfw3.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_glfw.h"
+#include "nlohmann/json.hpp"
 
 #include "Fonts/Roboto_Regular.h"
 #include "Fonts/Roboto_Bold.h"
 #include "Fonts/Roboto_Italic.h"
 
 export module ImguiContext;
+
+void set_dark_theme_colors()
+{
+    constexpr auto color_from_bytes = [](uint8_t r, uint8_t g, uint8_t b)
+    {
+        return ImVec4((float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+    };
+
+    auto& style = ImGui::GetStyle();
+    ImVec4* colors = style.Colors;
+
+    const ImVec4 bg_color = color_from_bytes(37, 37, 38);
+    const ImVec4 light_bg_color = color_from_bytes(82, 82, 85);
+    const ImVec4 very_light_bg_color = color_from_bytes(90, 90, 95);
+
+    const ImVec4 panel_color = color_from_bytes(65, 65, 70);
+    const ImVec4 panel_hover_color = color_from_bytes(29, 151, 236);
+    const ImVec4 panel_active_color = color_from_bytes(0, 119, 200);
+
+    const ImVec4 text_color = color_from_bytes(255, 255, 255);
+    const ImVec4 text_disabled_color = color_from_bytes(151, 151, 151);
+    const ImVec4 border_color = color_from_bytes(78, 78, 78);
+
+    const ImVec4 check_color = color_from_bytes(255, 153, 0);
+
+
+    colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.1f, 0.1f, 1.0f };
+    colors[ImGuiCol_ChildBg] = bg_color;
+    colors[ImGuiCol_PopupBg] = bg_color;
+
+    colors[ImGuiCol_Border] = border_color;
+    colors[ImGuiCol_BorderShadow] = border_color;
+
+    colors[ImGuiCol_Header] = panel_color;
+    colors[ImGuiCol_HeaderHovered] = panel_hover_color;
+    colors[ImGuiCol_HeaderActive] = panel_active_color;
+
+    colors[ImGuiCol_Button] = panel_active_color;
+    colors[ImGuiCol_ButtonHovered] = light_bg_color;
+    colors[ImGuiCol_ButtonActive] = very_light_bg_color;
+
+    colors[ImGuiCol_FrameBg] = panel_color;
+    colors[ImGuiCol_FrameBgHovered] = panel_hover_color;
+    colors[ImGuiCol_FrameBgActive] = panel_active_color;
+
+    colors[ImGuiCol_Tab] = bg_color;
+    colors[ImGuiCol_TabHovered] = panel_hover_color;
+    colors[ImGuiCol_TabActive] = panel_active_color;
+    colors[ImGuiCol_TabUnfocused] = bg_color;
+    colors[ImGuiCol_TabUnfocusedActive] = panel_active_color;
+
+    colors[ImGuiCol_TitleBg] = bg_color;
+    colors[ImGuiCol_TitleBgActive] = panel_hover_color;
+    colors[ImGuiCol_TitleBgCollapsed] = bg_color;
+
+    colors[ImGuiCol_Text] = text_color;
+    colors[ImGuiCol_TextDisabled] = text_disabled_color;
+    colors[ImGuiCol_TextSelectedBg] = panel_active_color;
+
+    colors[ImGuiCol_MenuBarBg] = panel_color;
+
+    colors[ImGuiCol_ScrollbarBg] = panel_color;
+    colors[ImGuiCol_ScrollbarGrab] = very_light_bg_color;
+    colors[ImGuiCol_ScrollbarGrabHovered] = panel_hover_color;
+    colors[ImGuiCol_ScrollbarGrabActive] = panel_active_color;
+
+    colors[ImGuiCol_CheckMark] = check_color;
+
+    colors[ImGuiCol_SliderGrab] = panel_hover_color;
+    colors[ImGuiCol_SliderGrabActive] = panel_active_color;
+
+    colors[ImGuiCol_Separator] = border_color;
+    colors[ImGuiCol_SeparatorHovered] = border_color;
+    colors[ImGuiCol_SeparatorActive] = border_color;
+
+    colors[ImGuiCol_ResizeGrip] = bg_color;
+    colors[ImGuiCol_ResizeGripHovered] = panel_color;
+    colors[ImGuiCol_ResizeGripActive] = light_bg_color;
+
+    colors[ImGuiCol_PlotLines] = panel_active_color;
+    colors[ImGuiCol_PlotLinesHovered] = panel_hover_color;
+    colors[ImGuiCol_PlotHistogram] = panel_active_color;
+    colors[ImGuiCol_PlotHistogramHovered] = panel_hover_color;
+
+    colors[ImGuiCol_DragDropTarget] = bg_color;
+    colors[ImGuiCol_NavHighlight] = bg_color;
+    colors[ImGuiCol_DockingPreview] = panel_active_color;
+
+    style.WindowPadding = ImVec2(2, 2);
+}
+
+void set_light_theme_colors()
+{
+    constexpr auto color_from_bytes = [](uint8_t r, uint8_t g, uint8_t b)
+    {
+        return ImVec4((float)r / 255.0f, (float)g / 255.0f, (float)b / 255.0f, 1.0f);
+    };
+
+    ImGui::StyleColorsLight();
+
+    auto& style = ImGui::GetStyle();
+    ImVec4* colors = style.Colors;
+
+    const ImVec4 bg_color = color_from_bytes(230, 230, 230);
+    const ImVec4 light_bg_color = color_from_bytes(82, 82, 85);
+    const ImVec4 very_light_bg_color = color_from_bytes(90, 90, 95);
+
+    const ImVec4 panel_color = color_from_bytes(166, 199, 214);
+    const ImVec4 panel_hover_color = color_from_bytes(191, 224, 240);
+    const ImVec4 panel_active_color = color_from_bytes(140, 173, 189);
+
+    const ImVec4 text_color = color_from_bytes(79, 64, 61);
+    const ImVec4 text_disabled_color = color_from_bytes(150, 150, 150);
+    const ImVec4 border_color = color_from_bytes(190, 190, 190);
+
+    const ImVec4 title_bg_color = color_from_bytes(107, 191, 255);
+    const ImVec4 title_collapsed_color = color_from_bytes(102, 166, 204);
+
+    const ImVec4 button_color = color_from_bytes(253, 218, 44);
+    const ImVec4 button_hovered = color_from_bytes(107, 209, 255);
+    const ImVec4 button_active = color_from_bytes(184, 255, 255);
+
+    const ImVec4 menu_bg_color = color_from_bytes(189, 189, 240);
+
+    const ImVec4 check_color = color_from_bytes(111, 65, 65);;
+
+
+    colors[ImGuiCol_WindowBg] = ImVec4{ 0.95f, 0.94f, 0.92f, 1.0f };
+    colors[ImGuiCol_ChildBg] = bg_color;
+    colors[ImGuiCol_PopupBg] = bg_color;
+
+    colors[ImGuiCol_Border] = border_color;
+    colors[ImGuiCol_BorderShadow] = border_color;
+
+    colors[ImGuiCol_Header] = panel_color;
+    colors[ImGuiCol_HeaderHovered] = panel_hover_color;
+    colors[ImGuiCol_HeaderActive] = panel_active_color;
+
+    colors[ImGuiCol_Button] = button_color;
+    colors[ImGuiCol_ButtonHovered] = button_hovered;
+    colors[ImGuiCol_ButtonActive] = button_active;
+
+    colors[ImGuiCol_FrameBg] = panel_color;
+    colors[ImGuiCol_FrameBgHovered] = panel_hover_color;
+    colors[ImGuiCol_FrameBgActive] = panel_active_color;
+
+    colors[ImGuiCol_Tab] = bg_color;
+    colors[ImGuiCol_TabHovered] = panel_hover_color;
+    colors[ImGuiCol_TabActive] = panel_active_color;
+    colors[ImGuiCol_TabUnfocused] = bg_color;
+    colors[ImGuiCol_TabUnfocusedActive] = panel_active_color;
+
+    colors[ImGuiCol_TitleBg] = panel_hover_color;
+    colors[ImGuiCol_TitleBgActive] = title_bg_color;
+    colors[ImGuiCol_TitleBgCollapsed] = title_collapsed_color;
+
+    colors[ImGuiCol_Text] = text_color;
+    colors[ImGuiCol_TextDisabled] = text_disabled_color;
+    colors[ImGuiCol_TextSelectedBg] = panel_active_color;
+
+    colors[ImGuiCol_MenuBarBg] = menu_bg_color;
+
+    colors[ImGuiCol_ScrollbarBg] = panel_color;
+    colors[ImGuiCol_ScrollbarGrab] = light_bg_color;
+    colors[ImGuiCol_ScrollbarGrabHovered] = light_bg_color;
+    colors[ImGuiCol_ScrollbarGrabActive] = very_light_bg_color;
+
+    colors[ImGuiCol_CheckMark] = check_color;
+
+    colors[ImGuiCol_SliderGrab] = panel_hover_color;
+    colors[ImGuiCol_SliderGrabActive] = panel_active_color;
+
+    colors[ImGuiCol_Separator] = border_color;
+    colors[ImGuiCol_SeparatorHovered] = border_color;
+    colors[ImGuiCol_SeparatorActive] = border_color;
+
+    colors[ImGuiCol_DockingPreview] = ImVec4(0.0f, 0.47f, 0.78f, 1.0f);
+
+    style.WindowPadding = ImVec2(2, 2);
+}
+ 
+std::unordered_map <std::string, std::any> app_styles{ {"dark", set_dark_theme_colors}, {"light", set_light_theme_colors} };
 
 export struct ui_context
 {
@@ -27,9 +215,6 @@ export struct ui_context
         io.Fonts->AddFontFromMemoryCompressedTTF(g_RobotoItalic, g_RobotoItalic_size, 18.0f, &fontConfig);
         io.FontDefault = io.Fonts->AddFontFromMemoryCompressedTTF(g_RobotoRegular, g_RobotoRegular_size, 18.0f, &fontConfig);
 
-        // Setup Dear ImGui style
-        ImGui::StyleColorsDark();
-
         // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
         ImGuiStyle& style = ImGui::GetStyle();
         if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
@@ -38,7 +223,12 @@ export struct ui_context
             style.Colors[ImGuiCol_WindowBg].w = 1.0f;
         }
 
-        set_dark_theme_colors();
+        std::fstream json_config_file;
+        json_config_file.open("ConfigFile.json", std::ios::in);
+        nlohmann::json json_data = nlohmann::json::parse(json_config_file);
+        json_config_file.close();
+
+        std::any_cast <void (*) ()> (app_styles[json_data["GuiStyle"].get<std::string>()]) ();
 
         ImGui_ImplGlfw_InitForOpenGL(window, true);
         ImGui_ImplOpenGL3_Init("#version 400 core");
@@ -98,33 +288,5 @@ export struct ui_context
             ImGui::RenderPlatformWindowsDefault();
             glfwMakeContextCurrent(backup_current_context);
         }
-    }
-
-    void set_dark_theme_colors()
-    {
-        auto& colors = ImGui::GetStyle().Colors;
-        colors[ImGuiCol_WindowBg] = ImVec4{ 0.1f, 0.1f, 0.1f, 1.0f };
-
-        colors[ImGuiCol_Header] = ImVec4{ 0.2f, 0.2f, 0.2f, 1.0f };
-        colors[ImGuiCol_HeaderHovered] = ImVec4{ 0.3f, 0.3f, 0.3f, 1.0f };
-        colors[ImGuiCol_HeaderActive] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-
-        colors[ImGuiCol_Button] = ImVec4{ 0.2f, 0.2f, 0.2f, 1.0f };
-        colors[ImGuiCol_ButtonHovered] = ImVec4{ 0.3f, 0.3f, 0.3f, 1.0f };
-        colors[ImGuiCol_ButtonActive] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-
-        colors[ImGuiCol_FrameBg] = ImVec4{ 0.2f, 0.2f, 0.2f, 1.0f };
-        colors[ImGuiCol_FrameBgHovered] = ImVec4{ 0.3f, 0.3f, 0.3f, 1.0f };
-        colors[ImGuiCol_FrameBgActive] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-
-        colors[ImGuiCol_Tab] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-        colors[ImGuiCol_TabHovered] = ImVec4{ 0.38f, 0.38f, 0.38f, 1.0f };
-        colors[ImGuiCol_TabActive] = ImVec4{ 0.28f, 0.28f, 0.28f, 1.0f };
-        colors[ImGuiCol_TabUnfocused] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-        colors[ImGuiCol_TabUnfocusedActive] = ImVec4{ 0.2f, 0.2f, 0.2f, 1.0f };
-
-        colors[ImGuiCol_TitleBg] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-        colors[ImGuiCol_TitleBgActive] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
-        colors[ImGuiCol_TitleBgCollapsed] = ImVec4{ 0.15f, 0.15f, 0.15f, 1.0f };
     }
 };
